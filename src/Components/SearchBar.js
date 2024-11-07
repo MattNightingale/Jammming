60f7f570531dd3e81942a5e1bcf11adc68b8777d
Matt Nightingale <matt.snail@gmail.com>
2024-11-07 11:07:26 +0000

    CSS

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -14,12 +14,18 @@ function SearchBar(props) {
         setTerm(target.value);
       };
 
+      function keyUp(event) {
+        if (event.key === "Enter") {
+          this.passTerm()
+        }
+      };
+
       return (
       <div className={styles.SearchBar}>
         <input
           placeholder="Enter A Song, Album, or Artist"
           onChange={handleTermChange}
-          onSubmit={handleTermChange}
+          onKeyUp={keyUp}
         />
         <button className={styles.SearchButton} onClick={passTerm}>
           SEARCH
