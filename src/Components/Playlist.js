307b6cd8788a9459de68824a9720d6f5a3af9200
Matt Nightingale <matt.snail@gmail.com>
2026-01-18 17:18:52 +0000

    moved save button to top

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import styles from './Playlist.module.css';
-import Tracklist from './Tracklist';
+import React, { useState } from "react";
+import styles from "./Playlist.module.css";
+import Tracklist from "./Tracklist";
 
 function Playlist(props) {
-    function handleNameChange({ target }) {
-        props.onNameChange(target.value);
-    };
+  function handleNameChange({ target }) {
+    props.onNameChange(target.value);
+  }
 
-    return (
-        <div className={styles.Playlist}>
-            <input placeholder={"New Playlist Name"} onChange={handleNameChange}/>
-            <Tracklist 
-            userSearchResults={props.playlistTracks} 
-            onRemove={props.onRemove} 
-            isRemoval={true}
-            />
-            <button className={styles["Playlist-save"]} onClick={props.onSave}>
-                SAVE TO SPOTIFY
-            </button>
-        </div>
-    );
+  return (
+    <div className={styles.Playlist}>
+      <input placeholder={"New Playlist Name"} onChange={handleNameChange} />
+      <button className={styles["Playlist-save"]} onClick={props.onSave}>
+        SAVE TO SPOTIFY
+      </button>
+      <Tracklist
+        userSearchResults={props.playlistTracks}
+        onRemove={props.onRemove}
+        isRemoval={true}
+      />
+    </div>
+  );
 }
 
 export default Playlist;
